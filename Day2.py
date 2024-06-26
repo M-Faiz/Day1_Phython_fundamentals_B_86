744505e9602d5fe6b342f3efae0decbb86d5e5ea
M-Faiz <74181325+M-Faiz@users.noreply.github.com>
2024-06-26 22:26:08 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+#Intro to python programming!
+
+
+# In[ ]:
+
+
+#understanding variable in py
+
+
+# In[ ]:
+
+
+Rule1: no namespace while declaring variable in python
+
+
+# In[ ]:
+
+
+Rule2:a variable name should not start with a number **
+
+
+# In[ ]:
+
+
+Rule3: A variable should not contain any special characters - @$#"&
+
+
+# In[3]:
+
+
+1name = 'Faiz'
+
+
+# In[ ]:
+
+
+#Intro to datatypes in py:
+
+
+# In[ ]:
+
+
+1. string ===> str
+2. Number ( integers (int) and floats (decimals 0.1, 0.2))
+3. lists ===> list
+4. tuples ===> tuple
+5. dictionaries ==> dict
+
+
+# In[ ]:
+
+
+classification of string datatype:
+  1. mutable datatypes ==> which we can edit and alter ==> flexible in nature
+  2. immutable datatype==> which we can't be able to edit or later ==> fixed in nature
+
+
+# In[ ]:
+
+
+#intr to string datatype:
+
+defination: a string is a combination of series of characters declared in quotes
+
+classification: it is classified as immutable datatype
+
+how to declare the string datattpe----?
+
+1. single quote
+2. double quote
+
+
+# In[5]:
+
+
+name = 'Faiz'
+print (name)
+
+
+# In[7]:
+
+
+name2 = "Mujahid"
+print (name2)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#intro to string methods:
+
+
+# In[9]:
+
+
+type(name2)
+
+
+# In[ ]:
+
+
+#type will tell you which string
+
+
+# In[11]:
+
+
+fullname='mujahid faiz'
+print (fullname)
+
+
+# In[13]:
+
+
+print(fullname.upper())
+
+
+# In[15]:
+
+
+print(fullname.lower())
+
+
+# In[19]:
+
+
+print (fullname.title())
+
+
+# In[ ]:
+
+
+
+
